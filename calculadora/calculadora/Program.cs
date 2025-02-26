92185ca014ddf58c493ae5e3d1e7d70750f5f0f8
BS_silva <bettinasantossilva1234@gmail.com>
2025-02-26 19:51:04 -0300

    Adicionar arquivos de projeto.

---
+++ calculadora/calculadora/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace calculadora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escreva a operação que deseja fazer: (mult) ou (div)");
+            if()
+        }
+    }
+}
