3c1f3a0d9f8741c24384a3d1e7fa30e156401f3e
BS_silva <bettinasantossilva1234@gmail.com>
2025-02-26 21:18:55 -0300

    escolha de operação

--- calculadora/calculadora/Program.cs
+++ calculadora/calculadora/Program.cs
@@ -1,11 +1,37 @@
-﻿namespace calculadora
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace calculadora
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Escreva a operação que deseja fazer: (mult) ou (div)");
-            if()
+            double val1, val2;
+
+            Console.WriteLine("Escreva a operação que deseja fazer: (adi) ou (sub)");
+            string escolhaOperacao = Console.ReadLine();
+
+            Console.WriteLine("Digite o primeiro valor: ");
+            val1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo valor: ");
+            val2 = double.Parse(Console.ReadLine());
+
+
+            double sub = (int)val1 - val2;
+            double adi = (int)val1 + val2;
+
+            if (escolhaOperacao == "adi")
+            {
+                Console.WriteLine("A soma dos números é: " + adi);
+            }
+            else 
+            {
+                Console.WriteLine("A subtração dos números é: " + sub );
+            }
         }
+        
+
+
     }
 }
